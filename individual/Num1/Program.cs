b1075c796885e4eb8a02ed475365172b22d8027e
Iskatiboi <scottfranklinmaher@gmail.com>
2024-09-19 23:37:35 +0800

    Add files via upload

---
+++ individual/Num1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Num1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Enter side A: ");
+          double a = Convert.ToDouble(Console.ReadLine());
+
+          Console.WriteLine("Enter side b: ");
+          double b = Convert.ToDouble(Console.ReadLine());
+
+          Console.WriteLine("Enter side c: ");
+          double c = Convert.ToDouble(Console.ReadLine());
+
+          double area = Area(a, b, c);
+
+          Console.WriteLine("The  area of the triangle is: " + area);
+
+        }
+
+        static double Sides(double a, double b, double c)
+        {
+
+         return (a + b + c)/2;
+
+        }
+
+        static double Area (double a, double b, double c)
+        {
+          double s = Sides(a, b, c);
+          return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+
+
+    }
+}
---
+++ individual/Num2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Num2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Enter a Year: ");
+          int year = Convert.ToInt32(Console.ReadLine());
+          
+          string Leap = LeapYear(year);
+
+          Console.WriteLine("The year " + year + " is a leap year " + Leap);
+
+        }
+
+        static string LeapYear(int year)
+        {
+          if (year % 4 == 0)
+          {
+            if (year % 100 == 0)
+            {
+              if (year % 400 == 0)
+              {
+                return "is a leap year.";
+              }
+              else
+              {
+                return "is not a leap year.";
+              }
+            }
+            else
+            {
+              return "is a leap year.";
+            }
+          }
+          else 
+          {
+            return "is not a leap year.";
+          }
+        }
+
+
+
+       
+        
+    }
+}
